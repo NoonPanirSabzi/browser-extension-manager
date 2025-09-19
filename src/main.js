8a06b5a8dc2759f7ddfc0d9bae9eaab75ff789a1
Amin Forouzan <amin.pqr@gmail.com>
2025-09-19 15:11:35 +0330

    feat: update UI data on load or filterTab switch

--- src/main.js
+++ src/main.js
@@ -1,7 +1,70 @@
 const elements = {
   filterTabs: document.querySelectorAll(".filters-container > .filter-tab"),
+  ExtensionsContainer: document.getElementById("extentions-container"),
 };
 let activeFilterTab = document.querySelector(".filter-tab--selected");
+let appData = null;
+
+function showFilteredData(data) {
+  let HTML = "";
+  data.forEach((extension) => {
+    const name = extension.name;
+    const logo = extension.logo;
+    const description = extension.description;
+    const isActive = extension.isActive;
+    HTML += `
+<article class="ex-card">
+  <div class="ex-card__info">
+    <img
+      src="${logo}"
+      alt="${name} logo"
+      class="ex-card__img"
+    />
+    <div class="flex-clmn">
+      <h2 class="text-preset-2 ex-card__name">${name}</h2>
+      <p class="text-preset-5 ex-card__description">
+        ${description}
+      </p>
+    </div>
+  </div>
+  <div class="ex-card__actions">
+    <button type="button" class="ex-card-remove-btn text-preset-6">
+      Remove
+    </button>
+    <label class="toggle-switch">
+      <input type="checkbox"${isActive ? "checked" : ""} />
+      <span class="slider"></span>
+    </label>
+  </div>
+</article>
+    `;
+  });
+  elements.ExtensionsContainer.innerHTML = HTML;
+}
+
+function handleData() {
+  switch (activeFilterTab.id) {
+    case "filter-all":
+      showFilteredData(appData.filter((extension) => !extension.isRemoved));
+      break;
+    case "filter-active":
+      showFilteredData(appData.filter((extension) => extension.isActive));
+      break;
+    case "filter-inactive":
+      showFilteredData(appData.filter((extension) => !extension.isActive));
+      break;
+    case "filter-removed":
+      showFilteredData(appData.filter((extension) => extension.isRemoved));
+      break;
+  }
+}
+
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    appData = data;
+    handleData();
+  });
 
 // Components Logic
 elements.filterTabs.forEach((filterTab) => {
@@ -15,8 +78,7 @@ elements.filterTabs.forEach((filterTab) => {
         tab.classList.toggle("text-preset-4");
       });
       activeFilterTab = filterTab;
-      /* To be implemented: 
-      showTabContent(activeFilterTab.id); */
+      handleData();
     }
   });
 });
